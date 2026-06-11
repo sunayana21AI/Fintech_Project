8ef6b0b2f99e0d26e16073b6b62451fc0b80d434
Sunayana Saman <samansunayana4@gmail.com>
2026-06-11 12:24:33 +0530

    Final submission structure update

---
+++ scripts/compute_metrics.py
@@ -0,0 +1,17 @@
+# Performance Metrics Computation
+
+"""
+This script contains functions used in
+04_performance_analytics.ipynb
+
+Metrics:
+- CAGR
+- Sharpe Ratio
+- Sortino Ratio
+- Alpha
+- Beta
+- Maximum Drawdown
+"""
+
+import pandas as pd
+import numpy as np
